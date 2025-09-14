8d5e55c805952c6e5a7f6bc679011a2fbf6fd589
Wise Munene Mutuku <wisemunene99@gmail.com>
2025-09-14 17:07:28 +0300

    done again

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Your code goes here
+
 document.addEventListener("DOMContentLoaded", function () {
   const paragraph = document.getElementById("text");
   paragraph.textContent = "This is really cool!";
